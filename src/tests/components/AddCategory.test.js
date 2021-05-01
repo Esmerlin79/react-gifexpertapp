9dc8022da2f8b497f273f76867a04b80a8c38e1e
Esmerlin79 <esmerlin79@hotmail.com>
2021-05-01 11:30:14 -0400

    update Test

--- src/tests/components/AddCategory.test.js
+++ src/tests/components/AddCategory.test.js
@@ -1,24 +1,38 @@
+import '@testing-library/jest-dom';
 import AddCategory from "../../components/AddCategory";
 import { shallow } from 'enzyme';
 
+
 describe('Test <AddCategory />', () => {
 
-    const setcategoria = () => {};
+    const setcategoria =  jest.fn();
 
-    const wraper = shallow(
+    let wraper = shallow(
         <AddCategory setcategoria={ setcategoria }/>
     );
+    beforeEach(() =>{
+        jest.clearAllMocks();
+        wraper = shallow(
+            <AddCategory setcategoria={ setcategoria }/>
+        );
+    })    
 
     test('should be displayed correctly ', () => {
         expect( wraper ).toMatchSnapshot();
     })
     test('should change the text box ', () => {
         const input = wraper.find('input');
-         const value = 'a value';  
+        const value = 'a value';  
 
         input.simulate('change', { target: { value } });
 
         expect( wraper.find('p').text().trim() ).toBe( value )
+    })
+    
+    test("shouldn't call the submit function ", () => {
+        wraper.find('form').simulate('submit', { preventDefault(){} })
+
+        expect( setcategoria ).not.toHaveBeenCalled();
     })
     
 })
